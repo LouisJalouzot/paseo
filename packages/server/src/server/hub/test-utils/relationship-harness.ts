75d7f03cdf9239900c38d65bc03b0f342b135161
Mohamed Boudra <boudra.moha@gmail.com>
2026-08-09 02:39:21 +0200

    test(server): distinguish Hub execution sessions

--- packages/server/src/server/hub/test-utils/relationship-harness.ts
+++ packages/server/src/server/hub/test-utils/relationship-harness.ts
@@ -830,6 +830,10 @@ export class HubRelationshipHarness {
     );
   }
 
+  providerCreations(): number {
+    return this.codex.creations;
+  }
+
   executionProviderCreations(): number {
     return this.codex.createdConfigs.filter((config) => config.internal !== true).length;
   }
